28392ae5e37af32e84e3b32815babf6e0a51ea6e
typischmann <jianyu.bao@gmail.com>
2015-04-05 01:55:22 +0200

    rename the password column and add a entity for sys user

--- retail-crm-services/src/main/resources/sql/create_db.sql
+++ retail-crm-services/src/main/resources/sql/create_db.sql
@@ -350,15 +350,15 @@ constraint PK_STORE_WAREHOUSE primary key (ID)
 /*																*/
 /*==============================================================*/
 create table SYS_USERS (
-ID                   SERIAL,
-USER_NAME            VARCHAR(64)                    unique not null,
-USER_PASSWORD        TEXT                    		not null, --Here storing a encrypted password
-AUTHORIZATION_LEVEL  INTEGER						default 0, --The permission of the user
-USER_STATUS			 INTEGER						not null, --CREATED, VERIFIED, BLOCKED
-USER_TYPE			 INTEGER						not null, --Personal Customer, Organization Customer, Employee, Supplier
-START_DATE           DATE                           not null,
-END_DATE           	 DATE,
-DELTA_TS			 TIMESTAMP						default current_timestamp,
+ID                   			SERIAL,
+USER_NAME            			VARCHAR(64)                 unique not null,
+ENCRYPTED_USER_PASSWORD        	TEXT                  		not null, --Here storing a encrypted password
+AUTHORIZATION_LEVEL  			INTEGER						default 0, --The permission of the user
+USER_STATUS			 			INTEGER						not null, --CREATED, VERIFIED, BLOCKED
+USER_TYPE			 			INTEGER						not null, --Personal Customer, Organization Customer, Employee, Supplier
+START_DATE           			DATE                        not null,
+END_DATE           	 			DATE,
+DELTA_TS			 			TIMESTAMP					default current_timestamp,
 constraint PK_SYS_USERS primary key (ID)
 );
 
